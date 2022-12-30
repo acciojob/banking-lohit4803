ecbebddc7b486878ae2c77714af2ec62c93dedbf
lohit4803 <lohit4803@gmail.com>
2022-12-30 19:51:32 +0530

    11

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,20 @@
 package com.driver;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CurrentAccount extends BankAccount{
-    String tradeLicenseId; //consists of Uppercase English characters only
+    public String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
+        super(name,balance,5000);
+        this.tradeLicenseId=tradeLicenseId;
+        if(balance<5000){
+            throw new Exception("Insufficient Balance");
+        }
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +22,34 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        boolean isValid = true;
+        for (int i=0;i<tradeLicenseId.length()-1;i++){
+            if(tradeLicenseId.charAt(i)==tradeLicenseId.charAt(i+1))
+            {
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid == false) {
+
+            char[] c = tradeLicenseId.toCharArray();
+            Arrays.sort(c);
+
+            for (int i = 1; i < c.length; i++) {
+                if (c[i] == c[i - 1]) {
+                    throw new Exception("Valid License can not be generated");
+                }
+            }
+        }
 
     }
 
+
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
+    }
 }
